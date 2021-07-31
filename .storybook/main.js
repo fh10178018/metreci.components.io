4435a1a5e6795f77d6e15bd7dd4f48e1b18de6dc
Metreci <1206758827@qq.com>
2021-07-31 12:57:48 +0800

    :rocket:  Project initialization

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/stories/*.stories.@(mdx|js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
